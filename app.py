b626a17028b7c2150bdf2718aabbeab1ef3d9f0b
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-18 16:16:17 -0700

    add login route

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.after_request
 def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+  response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+  response.headers["Expires"] = 0
+  response.headers["Pragma"] = "no-cache"
+  return response
 
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -38,5 +38,42 @@ db = SQL("sqlite:///refs.db")
 @app.route("/")
 @login_required
 def index():
-    """Show rating of refs"""
-    return render_template("sorry.html", message="TODO")
+  """Show rating of refs"""
+  return render_template("sorry.html", message="TODO")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  """Log user in"""
+
+  # Forget any user_id
+  session.clear()
+
+  # User reached route via POST (as by submitting a form via POST)
+  if request.method == "POST":
+
+    # Ensure username was submitted
+    if not request.form.get("username"):
+      return render_template("sorry.html", message="Please, provide username")
+
+    # Ensure password was submitted
+    elif not request.form.get("password"):
+      return render_template("sorry.html", message="Please, provide password")
+
+    # Query database for username
+    rows = db.execute("SELECT * FROM users WHERE username = :username",
+                      username=request.form.get("username"))
+
+    # Ensure username exists and password is correct
+    if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+      return render_template("sorry.html", message="Wrong username or password")
+
+    # Remember which user has logged in
+    session["user_id"] = rows[0]["id"]
+
+    # Redirect user to home page
+    return redirect("/")
+
+  # User reached route via GET (as by clicking a link or via redirect)
+  else:
+    return render_template("login.html")
